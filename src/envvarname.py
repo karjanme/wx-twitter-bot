c2fb9c54de540e767c48fb9b3787ed2bde3db8ad
jnsnkrllive <6225628+jnsnkrllive@users.noreply.github.com>
2020-12-10 22:07:55 -0800

    Add initial base application code

---
+++ src/envvarname.py
@@ -0,0 +1,17 @@
+from enum import Enum, auto, unique
+
+
+@unique
+class EnvVarName(Enum):
+    """
+    Environment Variables that are used by this project.
+    """
+
+    def _generate_next_value_(name, start, count, last_values):
+        return name
+
+    # TODO: Documentation
+    TWITTER_CONSUMER_KEY = auto()
+    TWITTER_CONSUMER_SECRET = auto()
+    TWITTER_ACCESS_TOKEN = auto()
+    TWITTER_ACCESS_TOKEN_SECRET = auto()
