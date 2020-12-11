6dd432d6255991834bab0791669de8b02d7048b0
Karl Jansen <6225628+karjanme@users.noreply.github.com>
2020-12-10 22:07:55 -0800

    Add initial base application code

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wxtwitterbot-jnsnkrllive",
+    version="0.0.1",
+    author="Karl Jansen",
+    author_email="jnsnkrl@live.com",
+    description="Wx Twitter Bot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jnsnkrllive/wx-twitter-bot",
+    package_dir={'wxtwitterbot': 'src'},
+    packages=['wxtwitterbot'],
+    python_requires='>=3.7',
+)
