c6104e0511c2f437902b42c8df91ec257a776242
Karl Jansen <6225628+karjanme@users.noreply.github.com>
2020-12-31 09:14:03 -0800

    Enable tweeting

--- src/main.py
+++ src/main.py
@@ -9,7 +9,6 @@ if sys.version_info < MIN_PYTHON:
     sys.exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
 
 from envvarname import EnvVarName
-from tweepy import API, OAuthHandler
 from tasks.solartime import SolarTimeTask
 from util import getEnvVar, isEmpty, loadEnvVars
 
@@ -39,49 +38,6 @@ def createLogger():
     return logging.getLogger()
 
 
-def createTwitterAPI():
-    LOGGER.debug("Creating the Twitter API")
-
-    consumer_key = getEnvVar(EnvVarName.TWITTER_CONSUMER_KEY)
-    if (isEmpty(consumer_key)):
-        message = "Environment Variable " + EnvVarName.TWITTER_CONSUMER_KEY + " is not set"
-        LOGGER.error(message)
-        raise RuntimeError(message)
-
-    consumer_secret = getEnvVar(EnvVarName.TWITTER_CONSUMER_SECRET)
-    if (isEmpty(consumer_secret)):
-        message = "Environment Variable " + EnvVarName.TWITTER_CONSUMER_SECRET + " is not set"
-        LOGGER.error(message)
-        raise RuntimeError(message)
-
-    access_token = getEnvVar(EnvVarName.TWITTER_ACCESS_TOKEN)
-    if (isEmpty(access_token)):
-        message = "Environment Variable " + EnvVarName.TWITTER_ACCESS_TOKEN + " is not set"
-        LOGGER.error(message)
-        raise RuntimeError(message)
-
-    access_token_secret = getEnvVar(EnvVarName.TWITTER_ACCESS_TOKEN_SECRET)
-    if (isEmpty(access_token_secret)):
-        message = "Environment Variable "+EnvVarName.TWITTER_ACCESS_TOKEN_SECRET + " is not set"
-        LOGGER.error(message)
-        raise RuntimeError(message)
-
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = API(auth,
-        wait_on_rate_limit=True,
-        wait_on_rate_limit_notify=True)
-
-    try:
-        api.verify_credentials()
-    except Exception as e:
-        LOGGER.error("Error creating Twitter API", exc_info=True)
-        raise e
-
-    LOGGER.info("Twitter API created successfully")
-    return api
-
-
 def sigintHandler(sig, frame):
     LOGGER.info("Shutting down, goodbye!")
     sys.exit(0)
@@ -96,7 +52,7 @@ loadEnvVars()
 LOGGER = createLogger()  # Requires that environment variables are loaded
 threading.excepthook = threadExceptionHook
 
-twitterAPI = createTwitterAPI()
+LOGGER.info("Application startup complete!")
 
 SolarTimeTask()
 
