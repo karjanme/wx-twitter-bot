ca537c4c56952c364fac03e459fe5006e1bec0ff
Karl Jansen <6225628+karjanme@users.noreply.github.com>
2021-01-25 21:17:09 -0800

    Improve setup script

--- setup.py
+++ setup.py
@@ -8,11 +8,26 @@ setuptools.setup(
     version="0.1.0",
     author="Karl Jansen",
     author_email="jnsnkrl@live.com",
+    license="MIT",
     description="Wx Twitter Bot",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jnsnkrllive/wx-twitter-bot",
     package_dir={'wxtwitterbot': 'src'},
-    packages=['wxtwitterbot'],
+    packages=['wxtwitterbot', 'wxtwitterbot.tasks'],
     python_requires='>=3.8',
+    install_requires=[
+        "astral",
+        "python-dotenv",
+        "pytz",
+        "tweepy",
+        "requests"
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
 )
