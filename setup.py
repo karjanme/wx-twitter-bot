dd3bac9f3774ca2431fad2319b3d7354b381f4ec
Karl Jansen <6225628+karjanme@users.noreply.github.com>
2022-09-17 22:51:59 -0700

    Increment Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wxtwitterbot",
-    version="1.0.3-beta",
+    version="1.0.4-beta",
     author="Karl Jansen",
     author_email="jnsnkrl@live.com",
     license="MIT",
